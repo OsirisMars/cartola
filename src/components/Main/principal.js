081b4cf4b894d3dc50a46fbc96714c082e9a8dc1
OsirisMars <gtmfilho@gmail.com>
2025-06-23 12:35:13 -0300

    alterações em botões, adições de flexbox para centralização dos botões

--- src/components/Main/principal.js
+++ src/components/Main/principal.js
@@ -5,7 +5,11 @@ export default function Principal(){
         <>
             <section className="teste">
                 <div>
-                    <button className='time'>Teste 1.0</button>
+                    <button className='time'>Liga 1</button>
+                    <button className='time'>Liga 2</button>
+                </div>
+                <div>
+                    <button className='time'>Liga 1</button>
                 </div>
             </section>
         </>
